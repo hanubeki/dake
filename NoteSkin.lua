4b1d4f5588d7ef656308b8751b1bfd8401072c88
hanubeki <hanubeki@gmail.com>
2020-09-17 16:36:22 +0900

    new extras system and some code changes

--- NoteSkin.lua
+++ NoteSkin.lua
@@ -7,6 +7,12 @@ local function BrightColor(col)
 	return {(col[1] + 2) / 3, (col[2] + 2) / 3, (col[3] + 2) / 3, col[4]}
 end
 
+-- [ja] Extras用変数テーブル
+-- Flat (bool): colored by column
+-- Routine (bool): colored by player number (actually not working on routine because of my mistake in sm5)
+-- HoldType (string): replace holds by specified name
+ret.HanubekiExtras = {}
+
 -- [ja] 各ボタンのリダイレクトテーブル
 ret.RedirTable =
 {
@@ -317,6 +323,202 @@ local ZoomTable = {
 }
 setmetatable(ZoomTable, FallbackOne)
 
+local ColorTable = {
+--[[
+	 0: Red
+	 2: Blue
+	 4: Green
+	 6: Yellow
+	 8: Purple
+	10: Teal
+	12: Magenta
+	14: Seafoam
+	16: Gray
+	18: Orange
+	20: Reserved
+	22: Reserved
+	24: Reserved
+	26: Reserved
+	28: Reserved
+	30: Reserved
+--]]
+	["dance"] = {
+		["Left"] = 8,
+		["UpLeft"] = 8,
+		["Down"] = 8,
+		["Up"] = 8,
+		["UpRight"] = 8,
+		["Right"] = 8,
+	},
+--[[
+	["dance"] = { -- Panel Color
+		["Left"] = 2,
+		["UpLeft"] = 4,
+		["Down"] = 0,
+		["Up"] = 0,
+		["UpRight"] = 4,
+		["Right"] = 2,
+	},
+--]]
+	["pump"] = {
+		["DownLeft"] = 2,
+		["UpLeft"] = 18,
+		["Center"] = 6,
+		["UpRight"] = 18,
+		["DownRight"] = 2,
+	},
+--[[
+	["pump"] = { -- pop'n stage Color
+		["DownLeft"] = 4,
+		["UpLeft"] = 6,
+		["Center"] = 0,
+		["UpRight"] = 2,
+		["DownRight"] = 16,
+	},
+--]]
+	["para"] = {
+		["Left"] = 12,
+		["UpLeft"] = 12,
+		["Up"] = 12,
+		["UpRight"] = 12,
+		["Right"] = 12,
+	},
+	["techno"] = {
+		["DownLeft"] = 2,
+		["Left"] = 4,
+		["UpLeft"] = 0,
+		["Down"] = 6,
+		["Center"] = 12,
+		["Up"] = 6,
+		["UpRight"] = 0,
+		["Right"] = 4,
+		["DownRight"] = 2,
+	},
+	["smx"] = { -- I'm not sure
+		["Left"] = 2,
+		["Down"] = 0,
+		["Center"] = 12,
+		["Up"] = 0,
+		["Right"] = 2,
+	},
+	["ez2"] = {
+		["FootUpLeft"] = 0,
+		["HandLrLeft"] = 4,
+		["HandUpLeft"] = 2,
+		["FootDown"] = 6,
+		["HandUpRight"] = 2,
+		["HandLrRight"] = 4,
+		["FootUpRight"] = 0,
+	},
+	["maniax"] = {
+		["HandLrLeft"] = 2,
+		["HandUpLeft"] = 12,
+		["HandUpRight"] = 12,
+		["HandLrRight"] = 2,
+	},
+	["ds3ddx"] = {
+		["HandLeft"] = 18,
+		["FootDownLeft"] = 2,
+		["FootUpLeft"] = 0,
+		["HandUp"] = 4,
+		["HandDown"] = 4,
+		["FootUpRight"] = 0,
+		["FootDownRight"] = 2,
+		["HandRight"] = 18,
+	},
+	["kb7"] = {
+		["Key1"] = 16,
+		["Key2"] = 2,
+		["Key3"] = 16,
+		["Key4"] = 18,
+		["Key5"] = 16,
+		["Key6"] = 2,
+		["Key7"] = 16,
+	},
+	["beat"] = {
+		["Key1"] = 16,
+		["Key2"] = 2,
+		["Key3"] = 16,
+		["Key4"] = 2,
+		["Key5"] = 16,
+		["Key6"] = 2,
+		["Key7"] = 16,
+		["scratch"] = 0,
+		["scratch up"] = 0,
+		["scratch down"] = 0,
+	},
+	["popn"] = {
+		["Left White"] = 16,
+		["Left Yellow"] = 6,
+		["Left Green"] = 4,
+		["Left Blue"] = 2,
+		["Red"] = 0,
+		["Right Blue"] = 2,
+		["Right Green"] = 4,
+		["Right Yellow"] = 6,
+		["Right White"] = 16,
+	},
+	["kickbox"] = {
+		["DownLeftFoot"]  = 2, -- Down: blue
+		["AnyLeftFoot"]   = 6, -- Any:  yellow
+		["UpLeftFoot"]    = 0, -- Up:   red
+		["UpLeftFist"]    = 0,
+		["AnyLeftFist"]   = 6,
+		["DownLeftFist"]  = 2,
+		["DownRightFist"] = 2,
+		["AnyRightFist"]  = 6,
+		["UpRightFist"]   = 0,
+		["UpRightFoot"]   = 0,
+		["AnyRightFoot"]  = 6,
+		["DownRightFoot"] = 2,
+	},
+	["guitar"] = {
+		["Fret 1"] = 6,
+		["Fret 2"] = 8,
+		["Fret 3"] = 4,
+		["Fret 4"] = 0,
+		["Fret 5"] = 2,
+		["Fret 6"] = 16,
+		["Strum Up"] = 16,
+		["Strum Down"] = 16,
+	},
+	["gddm"] = {
+		["Crash"]        = 12,
+		["Hi-Hat"]       = 2,
+		["Hi-Hat Pedal"] = 12,
+		["Snare"]        = 6,
+		["Tom1"]         = 4,
+		["Kick"]         = 8,
+		["Tom2"]         = 0,
+		["Tom3"]         = 18,
+		["Ride"]         = 2,
+	},
+	["lights"] = { -- isn't game
+		["MarqueeUpLeft"] = 0,
+		["MarqueeUpRight"] = 0,
+		["MarqueeLrLeft"] = 0,
+		["MarqueeLrRight"] = 0,
+		["ButtonsLeft"] = 0,
+		["ButtonsRight"] = 0,
+		["BassLeft"] = 0,
+		["BassRight"] = 0,
+	},
+	["Routine"] = { -- for Routine mode
+		["PlayerNumber_P1"] = 12,
+		["PlayerNumber_P2"] = 2,
+		["PlayerNumber_P3"] = 6,
+		["PlayerNumber_P4"] = 4,
+	},
+}
+local ColorTableMeta = {
+	__index = function(table, key, value)
+		local dummy = {}
+		setmetatable(dummy, FallbackZero)
+		return dummy
+	end
+}
+setmetatable(ColorTable, ColorTableMeta)
+
 ret.Redir = function(sButton, sElement)
 	-- Instead of separate hold heads, use the tap note graphics.
 	if sElement == "Hold Head Inactive" or
@@ -425,6 +627,14 @@ local function func()
 	local t = Def.ActorFrame {}
 
 	if sElementToLoad == "Tap Note" then
+		local color = 0
+
+		if ret.HanubekiExtras.Routine then
+			color = ColorTable["Routine"][pn]
+		elseif ret.HanubekiExtras.Flat then
+			color = ColorTable[game][sButton]
+		end
+
 		if sButtonToLoad == "Pedal" then
 			t[#t+1] = Def.Sprite {
 				Texture = NOTESKIN:GetPath("_common", "underlay foot"),
@@ -442,7 +652,7 @@ local function func()
 
 		t[#t+1] = Def.Sprite {
 			Texture = NOTESKIN:GetPath(TapRedir[sButtonToLoad], "tap note"),
-			Frames = {{Frame = 0, Delay = 1}},
+			Frames = {{Frame = color, Delay = 1}},
 			InitCommand = function (self) self:rotationy(rotY):rotationz(rotZ) end,
 		}
 
@@ -482,13 +692,13 @@ local function func()
 	elseif string.find(sElementToLoad, "Hold .*cap") or
 	       string.find(sElementToLoad, "Roll .*cap") then
 		t = Def.Sprite {
-			Texture = NOTESKIN:GetPath(HoldCapRedir[sButtonToLoad], string.lower(sElementToLoad)),
+			Texture = NOTESKIN:GetPath(ret.HanubekiExtras.HoldType or HoldCapRedir[sButtonToLoad], string.lower(sElementToLoad)),
 			Frames = {{Frame = 0, Delay = 1}},
 		}
 	elseif string.find(sElementToLoad, "Hold Body") or
 	       string.find(sElementToLoad, "Roll Body") then
 		t = Def.Sprite {
-			Texture = NOTESKIN:GetPath(HoldBodyRedir[sButtonToLoad], string.lower(sElementToLoad)),
+			Texture = NOTESKIN:GetPath(ret.HanubekiExtras.HoldType or HoldBodyRedir[sButtonToLoad], string.lower(sElementToLoad)),
 			Frames = {{Frame = 0, Delay = 1}},
 		}
 	elseif sElementToLoad == "Receptor" then
@@ -526,27 +736,31 @@ local function func()
 		t[#t+1] = Def.Sprite {
 			Texture = NOTESKIN:GetPath(TapRedir[sButtonToLoad], "receptor flash"),
 			Frames = {{Frame = 0, Delay = 1}},
-			InitCommand = function (self) self:rotationy(rotY):rotationz(rotZ):diffusealpha(0) end,
-			PressCommand = function (self) self:finishtweening():zoom(1):blend("BlendMode_Add"):diffusealpha(0.6) end,
+			InitCommand = function (self) self:blend("BlendMode_Add"):rotationy(rotY):rotationz(rotZ):diffusealpha(0) end,
+			PressCommand = function (self) self:finishtweening():zoom(1):diffusealpha(0.6) end,
 			LiftCommand = function (self) self:finishtweening():decelerate(0.1):diffusealpha(0):zoom(1.2) end,
 			NoneCommand = function (self) self:finishtweening():zoom(0.85):diffusealpha(0.9):linear(0.15):diffusealpha(1):zoom(1) end,
 		}
 	elseif sElementToLoad == "Explosion" then
+		local glowCommands = function(actor, color)
+			return actor:finishtweening():diffuse(color):sleep(0.1):decelerate(0.2):diffusealpha(0)
+		end
+
 		t[#t+1] = Def.Sprite {
 			Texture = NOTESKIN:GetPath(TapRedir[sButtonToLoad], "glow"),
 			Frames = {{Frame = 0, Delay = 1}},
 			InitCommand = function (self) self:rotationy(rotY):rotationz(rotZ):diffusealpha(0) end,
-			W5Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_W5")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W4Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_W4")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W3Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_W3")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W2Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_W2")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W1Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_W1")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW5Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_ProW5")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW4Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_ProW4")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW3Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_ProW3")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW2Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_ProW2")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW1Command = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_ProW1")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			HeldCommand = function (self) self:finishtweening():diffuse(JudgmentLineToColor("JudgmentLine_Held")):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
+			W5Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_W5")) end,
+			W4Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_W4")) end,
+			W3Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_W3")) end,
+			W2Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_W2")) end,
+			W1Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_W1")) end,
+			ProW5Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_ProW5")) end,
+			ProW4Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_ProW4")) end,
+			ProW3Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_ProW3")) end,
+			ProW2Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_ProW2")) end,
+			ProW1Command = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_ProW1")) end,
+			HeldCommand = function (self) glowCommands(self, JudgmentLineToColor("JudgmentLine_Held")) end,
 			JudgmentCommand = function (self) self:finishtweening() end,
 			-- BrightCommand = function (self) self:visible(false) end,
 			-- DimCommand = function (self) self:visible(true) end,
@@ -555,17 +769,17 @@ local function func()
 			Texture = NOTESKIN:GetPath("_common", "bright"),
 			Frames = {{Frame = 0, Delay = 1}},
 			InitCommand = function (self) self:zoom(zoomValue):diffusealpha(0) end,
-			W5Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_W5"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W4Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_W4"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W3Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_W3"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W2Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_W2"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			W1Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_W1"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW5Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_ProW5"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW4Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_ProW4"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW3Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_ProW3"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW2Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_ProW2"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			ProW1Command = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_ProW1"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
-			HeldCommand = function (self) self:finishtweening():diffuse(BrightColor(JudgmentLineToColor("JudgmentLine_Held"))):sleep(0.1):decelerate(0.2):diffusealpha(0) end,
+			W5Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_W5"))) end,
+			W4Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_W4"))) end,
+			W3Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_W3"))) end,
+			W2Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_W2"))) end,
+			W1Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_W1"))) end,
+			ProW5Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_ProW5"))) end,
+			ProW4Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_ProW4"))) end,
+			ProW3Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_ProW3"))) end,
+			ProW2Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_ProW2"))) end,
+			ProW1Command = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_ProW1"))) end,
+			HeldCommand = function (self) glowCommands(self, BrightColor(JudgmentLineToColor("JudgmentLine_Held"))) end,
 			JudgmentCommand = function (self) self:finishtweening() end,
 			BrightCommand = function (self) self:visible(true) end,
 			DimCommand = function (self) self:visible(false) end,
@@ -575,8 +789,8 @@ local function func()
 			Frames = {{Frame = 0, Delay = 1}},
 			HoldingOnCommand = function (self) self:diffusealpha(1):glowshift():effectcolor1({1, 1, 1, 0}):effectcolor2({1, 1, 1, 0.7}):effectperiod(0.1) end,
 			HoldingOffCommand = function (self) self:diffusealpha(0) end,
-			RollOnCommand = function (self) self:diffusealpha(1):glowshift():effectcolor1({1, 1, 1, 0}):effectcolor2({1, 1, 1, 0.7}):effectperiod(0.1) end,
-			RollOffCommand = function (self) self:diffusealpha(0) end,
+			RollOnCommand = function (self) self:playcommand("HoldingOn") end,
+			RollOffCommand = function (self) self:playcommand("HoldingOff") end,
 			InitCommand = function (self) self:playcommand("HoldingOff"):finishtweening() end,
 		}
 		t[#t+1] = Def.Sprite {
